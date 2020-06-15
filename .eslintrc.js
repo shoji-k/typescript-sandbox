9f77fa626424335f5c97056aa0b59604238377aa
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-06-15 18:13:59 +0900

    brush up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,14 @@
 module.exports = {
-    "root": true,
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": './tsconfig.json'
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "prettier"
-    ],
-    "rules": {
-    }
-};
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {},
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 console.log('hey')
+
+const a = 1 + 2
+const b = a + 3
+const c = {
+  apple: a,
+  banana: b,
+}
+const d = c.apple * 4
+console.log(a, b, c, d)
